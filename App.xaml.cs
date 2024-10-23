9c92ed50a3846a5db7c74cd038af899affc455be
bouro <bouro@POTATO>
2024-10-23 12:44:56 +0200

    Ajoutez des fichiers projet.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Unlock
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
